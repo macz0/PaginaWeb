ffdd9b0735d3c5661525f5dc4ec90c53ace020cf
Nea <macz0@hotmail.com>
2021-08-25 21:49:48 -0500

    pagina

---
+++ Codigo.js
@@ -0,0 +1,19 @@
+const objeto = {
+    nombre: "Luis",
+    imprimir: function () {
+        console.log("Nombre:", this.nombre) 
+    }
+}
+const aprendiendo = () => console.log("Aprendiendo JavaScript 2")
+
+aprendiendo();
+/*
+for (let i = 0; i < carro.length; i++) {
+    console.log("Producto: " + carro[i].nombre + ", precio:", carro[i].precio + ".");
+}
+
+carro.forEach(function (producto) {
+    console.log(`${producto.nombre} , precio: ${producto.precio}.`);
+})*/
+
+
